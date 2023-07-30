a9f582c7e6de3a4beae853f71c2eb61653f682d2
EC2 Default User <ec2-user@ip-172-31-4-234.ec2.internal>
2023-07-30 13:33:35 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,10 +3,11 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
+  has_many :favorited_users, through: :favorites, source: :user
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
